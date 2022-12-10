40103e8161dc569459d4605190fc2df7546fdbb4
jamesobed <jamesobed4@gmail.com>
2022-12-10 19:06:38 +0100

    fixed image overlap

--- src/app.ts
+++ src/app.ts
@@ -32,6 +32,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Hotel API");
+});
 app.use("/hotels", listingsRouter);
 app.use("/brand", brandsRouter);
 
